0729b8a662ad25eafa61496d87b0623a9f45a12c
Oswald Gerald <moswaldgerald@gmail.com>
2022-07-26 05:42:33 +0300

    pushed first file

---
+++ ML.py
@@ -0,0 +1,9 @@
+import statistics as st
+
+data = [1, 2, 3, 4, 5, 6]
+mean = st.mean(data)
+mode = st.mode(data)
+median = st.median(data)
+variance = st.variance(data)
+std = st.stdev(data)
+ # when printing all the variables will display the calculations accordingly
